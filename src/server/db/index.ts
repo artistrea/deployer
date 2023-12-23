9b68b3f7aec721017f54c2ed4d06fb9e6a6cf66d
artistrea <apiravesi@gmail.com>
2023-12-23 10:45:27 -0300

    little refactor

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,9 +2,12 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import { env } from "~/env";
 import * as schema from "./schema";
+import { registerService } from "~/utils/registerService";
 
 const connection = await mysql.createConnection({
   uri: env.DATABASE_URL,
 });
 
-export const db = drizzle(connection, { schema: schema, mode: "default" });
+export const db = registerService("db", () =>
+  drizzle(connection, { schema: schema, mode: "default" }),
+);
---
+++ src/utils/registerService.ts
@@ -0,0 +1,13 @@
+export function registerService<T extends any, N extends string>(
+  name: N,
+  initFn: () => T,
+): T {
+  const globalForService = globalThis as Record<N, T>;
+  if (process.env.NODE_ENV === "development") {
+    if (!(name in global)) {
+      globalForService[name] = initFn();
+    }
+    return globalForService[name];
+  }
+  return initFn();
+}
